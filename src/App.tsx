1dcb50458eeb9ac07162d56e940a6f27e50414a0
Sergey Ushakov <sergey@ushakov.io>
2021-08-14 18:32:06 +0300

    added octokit

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,19 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Route, BrowserRouter } from "react-router-dom";
 
 import logo from "./logo.svg";
 import "./App.css";
+import { Octokit } from "octokit";
+
+const octokitClient = new Octokit();
 
 function App() {
+  useEffect(() => {
+    octokitClient.rest.users
+      .getByUsername({ username: "serushakov" })
+      .then((user) => console.log(user));
+  }, []);
+
   return (
     <BrowserRouter>
       <Route path="/">
